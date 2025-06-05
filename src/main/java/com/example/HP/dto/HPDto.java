9ee79a89b3702b64f98016afb147c22cfe4a4286
Maysa Leocadio <lmaysagomes@gmail.com>
2025-06-05 14:50:37 -0300

    feat: updating

--- src/main/java/com/example/HP/dto/HPDto.java
+++ src/main/java/com/example/HP/dto/HPDto.java
@@ -5,7 +5,6 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Getter
-@Setter
 @ToString
 @Builder
 @NoArgsConstructor
@@ -15,33 +14,4 @@ public class HPDto {
     private String name;
     private LocalDate age;
     private String house;
-
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getAge() {
-        return age;
-    }
-    public void setAge(LocalDate age) {
-        this.age = age;
-    }
-
-    public String getHouse() {
-        return house;
-    }
-    public void setAge(String house) {
-        this.house = house;
-    }
-
 }

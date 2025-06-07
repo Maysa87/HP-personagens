0843ddccfa1715e299b41f37fc5016cbfe2ab7bc
Maysa Leocadio <lmaysagomes@gmail.com>
2025-06-06 23:06:37 -0300

    feat: adding characters into the database

--- src/main/java/com/example/HP/repository/HPRepository.java
+++ src/main/java/com/example/HP/repository/HPRepository.java
@@ -1,5 +1,8 @@
 package com.example.HP.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,6 @@ import com.example.HP.entity.HPEntity;
 
 @Repository
 public interface HPRepository extends JpaRepository<HPEntity, String> {
+	List<HPEntity> findByNameContainingIgnoreCaseOrBornDateGreaterThanEqualOrHouseContainingIgnoreCase(String name,
+			LocalDate bornDate, String house);
 }
